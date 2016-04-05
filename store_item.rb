8482e2665c2903419439b5bfd1a2c659d9f83122
Anna Natorilla <annagaiel@gmail.com>
2016-04-04 20:41:38 -0700

    add the store_item.rb

---
+++ store_item.rb
@@ -0,0 +1,20 @@
+require 'pry'
+
+animals = [
+  dog = {
+    color:"black",
+    legs:4,
+    price:100
+  },
+  cat = {
+    color:"white",
+    legs:4,
+    price:200
+  },
+  pig = {
+    color:"pink",
+    legs:4,
+    price:300
+  }
+]
+
