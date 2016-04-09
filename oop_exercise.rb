8c4710e7184f36ff569cda7f524c6d243cba3187
Anna Natorilla <annagaiel@gmail.com>
2016-04-09 16:47:41 -0700

    Added options

--- oop_exercise.rb
+++ oop_exercise.rb
@@ -1,23 +1,12 @@
 require 'pry'
 
 class Card
-
-  def initialize(question, answer)
-    @question = question
-    @answer = answer
+  attr_reader :question, :answer, :options
+  def initialize(hash)
+    @question = hash[:question]
+    @answer = hash[:answer]
+    @options = hash[:options]
   end
-
-  def question
-    @question
-  end
-
-  def answer
-    @answer
-  end
-
-  # def check_answer
-
-  # end
 
 end
 
@@ -25,8 +14,8 @@ class Deck
 
   def initialize(data)
     @list_cards = []
-    data.each do |key, value|
-      @list_cards << Card.new(key, value)
+    data.each do |hash|
+      @list_cards << Card.new(hash)
     end
   end
 
@@ -38,21 +27,31 @@ class Deck
     @list_cards.pop
   end
 
-
-
 end
 
-trivia_data = {
-  "What is the capital of Illinois?" => "Springfield",
-  "Is Africa a country or a continent?" => "Continent",
-  "Tug of war was once an Olympic event. True or false?" => "True"
-}
+trivia_data = [
+  {
+    question:'What is the capital of Illinois?',
+    options:['a. Montgomery','b. Sacaramento','c. Springfield','d. Denver'],
+    answer: 'c'
+  },
+  {
+    question:'What is the capital of Arizona',
+    options:['a. Phoenix','b. Dover','c. Atlanta','d. Boise'],
+    answer: 'a'
+  },
+  {
+    question:'What is the capital of California',
+    options:['a. Sacramento','b. Dover','c. Atlanta','d. Boise'],
+    answer: 'a'
+  },
+              ]
 
 deck = Deck.new(trivia_data) # deck is an instance of the Deck class
-
 while deck.remaining_cards > 0
   card = deck.draw_card # card is an instance of the Card class
   puts card.question
+  puts "#{card.options.join(', ')}"
   user_answer = gets.chomp
   if user_answer.downcase == card.answer.downcase
     puts "Correct!"
